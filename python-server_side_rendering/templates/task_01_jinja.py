d065828262fd7c735a4e960dfe4db7d8f02a46c8
Fatima <128232490+salalifatima@users.noreply.github.com>
2025-04-02 13:14:12 +0400

    Update task_01_jinja.py

--- python-server_side_rendering/templates/task_01_jinja.py
+++ python-server_side_rendering/templates/task_01_jinja.py
@@ -1,49 +1,18 @@
-#!/usr/bin/python3
-import os
+from flask import Flask, render_template
 
+app = Flask(__name__)
 
-def generate_invitations(template, attendees):
-    if not template:
-        print('Error: Template is empty, no output files generated.')
-        return
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-    if not attendees:
-        print('Error: No data provided, no output files generated.')
-        return
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
-    if not isinstance(template, str):
-        print('Error: Invalid input')
-        return
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
-    if (not isinstance(attendees, list) or
-            not all(isinstance(item, dict) for item in attendees)):
-        print('Error: Invalid input')
-        return
-
-    for index, attendee in enumerate(attendees, start=1):
-        template_schema = template
-        for key in ['name', 'event_title', 'event_date', 'event_location']:
-            placeholder = '{' + f'{key}' + '}'
-            value = attendee.get(key) or "N/A"
-            template_schema = template_schema.replace(placeholder, value)
-        if not os.path.exists(f'output_{index}.txt'):
-            with open(f'output_{index}.txt', 'w') as file:
-                file.write(template_schema)
-        else:
-            print('Error: file already exists')
-
-
-if __name__ == "__main__":
-
-    with open('template.txt', 'r') as file:
-        template_content = file.read()
-    # List of attendees
-    attendees = [
-        {"name": "Alice", "event_title": "Python Conference",
-         "event_date": "2023-07-15", "event_location": "New York"},
-        {"name": "Bob", "event_title": "Data Science Workshop",
-         "event_date": "2023-08-20", "event_location": "San Francisco"},
-        {"name": "Charlie", "event_title": "AI Summit",
-         "event_date": None, "event_location": "Boston"}
-    ]
-    generate_invitations(template_content, attendees)
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
